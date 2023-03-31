f6433fab712f59e01f872b27c290a1855d4ef5f8
Answed <schradearwed@gmail.com>
2023-03-31 10:47:26 +0200

    Added search list Added remove name from list

--- websideLogic.js
+++ websideLogic.js
@@ -1,14 +1,26 @@
+let namelist = ["Arwed", "Test"];
+let isNameinList = false;
 
 const btn = document.querySelector("button");
 
 console.log(btn);
 
-function getVal(val){
-    alert(val);
+function checkList(val){
+    namelist.forEach(function(name, index) {
+        if (val == name){
+            alert(val + " ist in der Liste")
+            namelist.splice(index);
+            isNameinList = true;
+        }
+        else {isNameinList = false}
+    })
+    if (!isNameinList){
+        alert("Der eingegebene Name ist nicht in der Liste")
+    }
 }
 
 btn.onclick = () =>{
     const val = document.querySelector("input").value;
-    getVal(val);
+    checkList(val);
 }
 
